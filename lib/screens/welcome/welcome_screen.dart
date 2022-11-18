788c4448837db2bb2866a9f703f5cd1672c53f00
Dinusha Rajapaksha <69702496+DinushaDinusha-Rajapaksha@users.noreply.github.com>
2022-11-18 16:40:25 +0530

    flutter quiz app

---
+++ lib/screens/welcome/welcome_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+import 'package:quiz_app/constants.dart';
+import 'package:quiz_app/screens/quiz/quiz_screen.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          //SvgPicture.asset("assets/icons/bg.jpg", fit: BoxFit.fill),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Spacer(flex: 2), //2/6
+                  Text(
+                    "Update your knowledge with MCQs",
+                    style: Theme.of(context).textTheme.headline4!.copyWith(
+                        color: Color.fromARGB(255, 11, 1, 48), fontWeight: FontWeight.bold),
+                  ),
+                  Spacer(), // 1/6
+                  TextField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Color.fromARGB(255, 114, 232, 250),
+                      hintText: "Enter your Full Name ",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
+                      ),
+                    ),
+                  ),
+                  Spacer(), // 1/6
+                  InkWell(
+                    onTap: () => Get.to(QuizScreen()),
+                    child: Container(
+                      width: double.infinity,
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.all(kDefaultPadding * 0.75), // 15
+                      decoration: BoxDecoration(
+                        gradient: kPrimaryGradient,
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
+                      ),
+                      child: Text(
+                        "Start your quiz here",
+                        style: Theme.of(context)
+                            .textTheme
+                            .button!
+                            .copyWith(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                  Spacer(flex: 2), // it will take 2/6 spaces
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
